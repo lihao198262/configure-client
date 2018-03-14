eb4c736d435b4f38b3d2633e97fd73f435271d6d
lihao198262 <lihao198262@163.com>
2018-03-14 17:40:16 +0800

    modify

--- src/main/java/com/lihao/configure/client/ConfigureClientApplication.java
+++ src/main/java/com/lihao/configure/client/ConfigureClientApplication.java
@@ -1,9 +1,13 @@
 package com.lihao.configure.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ContextIdApplicationContextInitializer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +19,17 @@ public class ConfigureClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigureClientApplication.class, args);
 	}
-	
+
 	@Value("${lihao}")
 	String lihao;
-	
+
+	@Autowired
+	private Environment env;
+
 	@RequestMapping("/")
-    public String home() {
-        return "Hello World! " + " " + lihao;
-    }
+	public String home(String key) {
+		if (key == null || key.isEmpty())
+			key = "spring.cloud.bus.id";
+		return "Hello World! " + " " + lihao + " " + key + "=" + env.getProperty(key);
+	}
 }
